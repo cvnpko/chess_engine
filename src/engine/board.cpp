201c64623656064f454dd1dfe0a4adff0c465654
Filip Sreckovic <filipsreckovic@gmail.com>
2025-05-29 16:52:33 +0200

    added pinned option to figure and the base for board flipping
    should probably be doing this in a branch, will start doing that after board flipping

--- src/engine/board.cpp
+++ src/engine/board.cpp
@@ -4,22 +4,22 @@ namespace engine
 {
     board::board()
     {
-        board_grid[0][0] = figure(figure_type::ROOK, figure_color::WHITE);
-        board_grid[0][7] = figure(figure_type::ROOK, figure_color::WHITE);
-        board_grid[7][0] = figure(figure_type::ROOK, figure_color::BLACK);
-        board_grid[7][7] = figure(figure_type::ROOK, figure_color::BLACK);
-        board_grid[0][1] = figure(figure_type::KNIGHT, figure_color::WHITE);
-        board_grid[0][6] = figure(figure_type::KNIGHT, figure_color::WHITE);
-        board_grid[7][1] = figure(figure_type::KNIGHT, figure_color::BLACK);
-        board_grid[7][6] = figure(figure_type::KNIGHT, figure_color::BLACK);
-        board_grid[0][2] = figure(figure_type::BISHOP, figure_color::WHITE);
-        board_grid[0][5] = figure(figure_type::BISHOP, figure_color::WHITE);
-        board_grid[7][2] = figure(figure_type::BISHOP, figure_color::BLACK);
-        board_grid[7][5] = figure(figure_type::BISHOP, figure_color::BLACK);
-        board_grid[0][3] = figure(figure_type::KING, figure_color::WHITE);
-        board_grid[0][4] = figure(figure_type::QUEEN, figure_color::WHITE);
-        board_grid[7][3] = figure(figure_type::KING, figure_color::BLACK);
-        board_grid[7][4] = figure(figure_type::QUEEN, figure_color::BLACK);
+        board_grid[0][0] = figure(figure_type::ROOK, figure_color::BLACK);
+        board_grid[0][7] = figure(figure_type::ROOK, figure_color::BLACK);
+        board_grid[7][0] = figure(figure_type::ROOK, figure_color::WHITE);
+        board_grid[7][7] = figure(figure_type::ROOK, figure_color::WHITE);
+        board_grid[0][1] = figure(figure_type::KNIGHT, figure_color::BLACK);
+        board_grid[0][6] = figure(figure_type::KNIGHT, figure_color::BLACK);
+        board_grid[7][1] = figure(figure_type::KNIGHT, figure_color::WHITE);
+        board_grid[7][6] = figure(figure_type::KNIGHT, figure_color::WHITE);
+        board_grid[0][2] = figure(figure_type::BISHOP, figure_color::BLACK);
+        board_grid[0][5] = figure(figure_type::BISHOP, figure_color::BLACK);
+        board_grid[7][2] = figure(figure_type::BISHOP, figure_color::WHITE);
+        board_grid[7][5] = figure(figure_type::BISHOP, figure_color::WHITE);
+        board_grid[0][4] = figure(figure_type::KING, figure_color::BLACK);
+        board_grid[0][3] = figure(figure_type::QUEEN, figure_color::BLACK);
+        board_grid[7][4] = figure(figure_type::KING, figure_color::WHITE);
+        board_grid[7][3] = figure(figure_type::QUEEN, figure_color::BLACK);
         for (int i = 0; i < 8; i++)
         {
             board_grid[1][i] = figure(figure_type::PAWN, figure_color::WHITE);
--- src/engine/figure.cpp
+++ src/engine/figure.cpp
@@ -2,10 +2,16 @@
 
 namespace engine
 {
-    figure::figure()
-        : type(figure_type::NONE), color(figure_color::NONE) {}
+    figure::figure() : type(figure_type::NONE), color(figure_color::NONE) 
+    {
+        pinned = false;
+    }
     figure::figure(figure_type type, figure_color color)
-        : type(type), color(color) {}
+    {
+        this->type = type;
+        this->color = color;
+        pinned = false;
+    }
 
     figure_type figure::get_type() const
     {
@@ -15,4 +21,14 @@ namespace engine
     {
         return color;
     }
+
+    bool figure::get_pinned() const
+    {
+        return pinned;
+    }
+    void figure::set_pinned(bool pinstatus)
+    {
+        pinned = pinstatus;
+    }
+
 }
