84638b22e9b8fabdaa21dd3d28bd9471452ba6fb
cvnpko <lazar.igob@gmail.com>
2025-05-30 13:02:09 +0200

    Add event_handler.hpp and event_handler.cpp files with initial implementation

---
+++ include/gui/event_handler.hpp
@@ -0,0 +1,28 @@
+#ifndef GUI_EVENT_HANDLER_HPP
+#define GUI_EVENT_HANDLER_HPP
+
+#include "engine/board.hpp"
+#include "gui/window.hpp"
+#include <SFML/Graphics.hpp>
+
+namespace gui
+{
+
+    class EventHandler
+    {
+    public:
+        EventHandler(engine::Board &board, gui::Window &window);
+        void processEvents();
+
+    private:
+        engine::Board &board;
+        gui::Window &window;
+        float squareSize = 100.0f;
+        bool pieceSelected = false;
+        sf::Vector2i selectedSquare;
+        void handleEvent(const sf::Event &event);
+        sf::Vector2i getBoardCoordinates(int x, int y) const;
+    };
+
+}
+#endif
---
+++ src/gui/event_handler.cpp
@@ -0,0 +1,22 @@
+#include "gui/event_handler.hpp"
+
+namespace gui
+{
+
+    EventHandler::EventHandler(engine::Board &b, gui::Window &w)
+        : board(b), window(w), pieceSelected(false)
+    {
+    }
+
+    void EventHandler::processEvents()
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+    }
+}
