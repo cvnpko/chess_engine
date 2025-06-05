d66a0add9a4f1c2cf1695da8cea7970d00ef1a2e
cvnpko <lazar.igob@gmail.com>
2025-06-05 11:51:26 +0200

    chore: add build directory to .gitignore for cross-platform compatibility

--- build/CMakeFiles/chess_engine.dir/src/engine/board.cpp.obj.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/chess_engine.dir/src/engine/board.cpp.obj: \
- D:/Projects/GitHub/chess_engine/src/engine/board.cpp \
- D:/Projects/GitHub/chess_engine/include/engine/board.hpp \
- D:/Projects/GitHub/chess_engine/include/engine/figure.hpp
--- build/CMakeFiles/chess_engine.dir/src/engine/figure.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/chess_engine.dir/src/engine/figure.cpp.obj: \
- D:/Projects/GitHub/chess_engine/src/engine/figure.cpp \
- D:/Projects/GitHub/chess_engine/include/engine/figure.hpp
