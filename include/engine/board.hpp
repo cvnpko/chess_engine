9f715d1d9af0d15a2292e63173d682eb1fc4c060
cvnpko <lazar.igob@gmail.com>
2025-06-12 13:22:21 +0200

    refactor: move logic for selected piece into board class

--- include/engine/board.hpp
+++ include/engine/board.hpp
@@ -5,35 +5,35 @@
 
 namespace engine
 {
-        enum class BoardSide
-        {
-                WHITE,
-                BLACK
-        };
-        class Board
-        {
-        public:
-                Board();
-                BoardSide getBoardSide() const;
-                Figure &getFigure(int x, int y);
-                void setBoardSide(BoardSide newSide);
-                void changeBoardSide();
-                bool validMove(int xStart, int yStart, int xEnd, int yEnd);
-                void update(int xStart, int yStart, int xEnd, int yEnd);
-                bool getIsSelected() const;
-                int getSelectedRow() const;
-                int getSelectedCol() const;
-                void deselect();
-                void select(const int x, const int y);
-                FigureColor getCurrentTurn() const;
+    enum class BoardSide
+    {
+        WHITE,
+        BLACK
+    };
+    class Board
+    {
+    public:
+        Board();
+        BoardSide getBoardSide() const;
+        Figure &getFigure(int x, int y);
+        void setBoardSide(BoardSide newSide);
+        void changeBoardSide();
+        bool validMove(int xStart, int yStart, int xEnd, int yEnd);
+        void update(int xStart, int yStart, int xEnd, int yEnd);
+        bool getIsSelected() const;
+        int getSelectedRow() const;
+        int getSelectedCol() const;
+        void deselect();
+        void select(const int x, const int y);
+        FigureColor getCurrentTurn() const;
 
-        private:
-                Figure boardGrid[8][8];
-                BoardSide boardSide;
-                FigureColor currentTurn;
-                int selectedRow, selectedCol;
-                bool isSelected;
-        };
+    private:
+        Figure boardGrid[8][8];
+        BoardSide boardSide;
+        FigureColor currentTurn;
+        int selectedRow, selectedCol;
+        bool isSelected;
+    };
 }
 
 #endif
